19c6e5947decdfb7a73eea74f7b840b14d77740f
anpdgr <ant-dgr@hotmail.com>
2020-04-28 01:53:43 +0700

    confuse

--- src/components/NavLO.vue
+++ src/components/NavLO.vue
@@ -34,18 +34,6 @@
               <a class="nav-link js-scroll-trigger" href="register">My profile</a>
             </li>
 
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">My profile</a>
-                    <a class="dropdown-item" href="#">Booking history</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Sign out</a>
-                </div>
-            </li>
-
             <li class="nav-item">
               <a v-on:click="check=0" class="nav-link js-scroll-trigger" href='/'>Sign out</a>
             </li>
