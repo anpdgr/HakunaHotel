888943160b64ca3cfd926f06c5b07da19441eaeb
WarakornInthong <kornkan_pokemon@hotmail.com>
2020-05-03 21:53:11 +0700

    install axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 //import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 
