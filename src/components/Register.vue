35da59f9571e4fa96ea9653998877a8cc108e4d9
WarakornInthong <kornkan_pokemon@hotmail.com>
2020-05-03 23:57:49 +0700

    regis can regis by axios

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,14 +1,17 @@
 <template>
   <body id="regis">
+    <!-- sign up box -->
     <div id="box">
       <b-card border-varient="Primary" class="container" style="padding:5px">
         <div id="left" class="panel panel-primary">
           <div class="panel-heading">
             <h3 class="panel-title">Sign up</h3>
           </div>
+          <!-- Form regis -->
+          <!-- User regis -->
           <b-form @submit="onSubmit">
             <br />
-
+            <!-- input email -->
             <b-form-group id="ipg-email">
               <b-form-input
                 id="ip-email"
@@ -19,6 +22,7 @@
               ></b-form-input>
             </b-form-group>
 
+            <!-- input User name -->
             <b-form-group id="ipg-uname">
               <b-form-input
                 id="ip-uname"
@@ -28,6 +32,7 @@
               ></b-form-input>
             </b-form-group>
 
+            <!-- input password -->
             <b-form-group id="ipg-pass">
               <b-form-input
                 type="password"
@@ -38,6 +43,7 @@
               ></b-form-input>
             </b-form-group>
 
+            <!-- input confirm password -->
             <b-form-group id="ipg-cpass">
               <b-form-input
                 type="password"
@@ -46,11 +52,15 @@
                 required
                 placeholder="Confirm your password"
               ></b-form-input>
+
+            <!-- Details regis -->
             </b-form-group>
             <div style="margin-top:30px; margin-bottom:30px"><hr /></div>
             <h5>Personal information</h5>
             <div style="margin-bottom:20px"></div>
             <b-form-group id="ipg-title">
+
+              <!-- radio selete name title -->
               <b-form-select
                 id="ip-title"
                 v-model="form.title"
@@ -59,6 +69,7 @@
               ></b-form-select>
             </b-form-group>
 
+            <!-- input first name -->
             <b-form-group id="ipg-fname">
               <b-form-input
                 id="ip-fname"
@@ -68,6 +79,7 @@
               ></b-form-input>
             </b-form-group>
 
+            <!-- input last name -->
             <b-form-group id="ipg-lname">
               <b-form-input
                 id="ip-lname"
@@ -77,6 +89,7 @@
               ></b-form-input>
             </b-form-group>
 
+            <!-- input birthday -->
             <b-form-group id="ipg-birthday">
               <b-form-datepicker
                 id="ip-birthday"
@@ -86,6 +99,7 @@
               ></b-form-datepicker>
             </b-form-group>
 
+            <!-- input telephone numeber -->
             <b-form-group id="ipg-tel">
               <b-form-input
                 id="ip-tel"
@@ -96,6 +110,7 @@
               ></b-form-input>
             </b-form-group>
 
+            <!-- input country -->
             <b-form-group id="ipg-country">
               <b-form-input
                 id="ip-country"
@@ -105,14 +120,22 @@
               ></b-form-input>
             </b-form-group>
 
+            <!-- Button -->
             <div style="float: left;">
+              
+              <!-- sign up button -->
               <b-button formtarget="_blank" type="submit" variant="primary"
                 >Sign up</b-button
               >
+
+              <!-- alert button -->
+              <b-button formtarget="_blank" @click="UseDev">dummy</b-button>
+
             </div>
           </b-form>
         </div>
         <br /><br />
+        <!-- return to home page -->
         <div id="back">
           <a href="/">Back to home</a>
         </div>
@@ -125,6 +148,7 @@
 export default {
   data() {
     return {
+      owo: false,
       form: {
         email: "",
         username: "",
@@ -132,19 +156,46 @@ export default {
         cpassword: "",
         tel: "",
         title: null,
+        name:"",
         fname: "",
         lname: "",
-        data: null,
-        country: null,
+        birthday: "",
+        // data: null,//??????? 
+        country: "",
       },
       title: [{ text: "Name title", value: null }, "Mr.", "Ms.", "Miss"],
       show: true,
+      // for DB
+      newUser: {
+        userid: "",
+        pass: "",
+        title: "",
+        cusname: "",
+        tel: "",
+        email: "",
+        country: ""
+      }
+      
     };
   },
   methods: {
+    // default user easy click
+    UseDev(){
+      this.form = {
+        email: "test@mail.com",
+        username: "testuser",
+        password: "password",
+        cpassword: "password",
+        tel: "123456",
+        title: "Mr.",
+        fname: "Unknow",
+        lname: "Unknow",
+        birthday: "2020-05-05",
+        country: "Astera",
+      }
+    },
     onSubmit(evt) {
       evt.preventDefault();
-      //alert(JSON.stringify(this.form))
       if (this.form.password.length < 7) {
         this.makeToast("danger", "Minimum length for password is 7");
       } else if (this.form.password !== this.form.cpassword) {
@@ -153,7 +204,11 @@ export default {
           "Your password and confirmation password do not match."
         );
       } else {
-        this.$router.push("login");
+        this.setNewUser();
+        this.addUser();
+        this.form.name = this.form.fname +" "+ this.form.lname;
+        alert(JSON.stringify(this.newUser))
+        // this.$router.push("login");
       }
     },
     makeToast(variant = null, text) {
@@ -163,6 +218,62 @@ export default {
         solid: true,
         toaster: "b-toaster-bottom-center",
       });
+    },
+    // set user from form
+    setNewUser(){
+      this.newUser= {
+        userid: this.form.username,
+        pass: this.form.password,
+        title: this.form.title,
+        cusname: this.form.fname + " " + this.form.lname,
+        tel: this.form.tel,
+        email: this.form.email,
+        country: this.form.country
+      }
+    },
+    // axios post data
+    addUser(){
+      var formData = this.toFormData(this.newUser);
+      this.axios
+        .post("http://hakuna-hotel.kmutt.me/phpapi/regis.php?action=add",formData)
+        .then(response => {
+          //set var to default
+          this.form = {
+            email: "",
+            username: "",
+            password: "",
+            cpassword: "",
+            tel: "",
+            title: null,
+            fname: "",
+            lname: "",
+            birthday: "",
+            country: "",
+          };
+          this.newUser= {
+            userid: "",
+            pass: "",
+            title: "",
+            cusname: "",
+            tel: "",
+            email: "",
+            country: ""
+          };
+          if(response.data.error){
+            console.log(response.data.error);
+          }
+          else{
+            console.log(response.data.message);
+          }
+        });
+    },
+    // make obj type FormData 
+    toFormData(obj){
+      var fd = new FormData();
+      for(var i in obj){
+        fd.append(i,obj[i]);
+      }
+      return fd;
     },
   },
 };
--- src/components/login.vue
+++ src/components/login.vue
@@ -60,7 +60,7 @@ export default {
   methods: {
     onSubmit(evt) {
       evt.preventDefault();
-      //alert(JSON.stringify(this.form))
+      // alert(JSON.stringify(this.form))
       this.$router.push("/");
     },
   },
