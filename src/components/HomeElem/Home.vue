8a6d0f5ad8e6ccc148e6f4c0ca734c4cbf42c853
WarakornInthong <kornkan_pokemon@hotmail.com>
2020-05-08 17:14:11 +0700

    file home aleady complete

--- src/components/HomeElem/Home.vue
+++ src/components/HomeElem/Home.vue
@@ -75,9 +75,9 @@
 import NavLogIn from "../Nav.vue";//+
 import NavLogOut from "../NavLO.vue";//+
 import slide from "./SlidePic.vue";//+
-import weltx from "./Weltext.vue";// ?
+import weltx from "./Weltext.vue";//+
 import checkavi from "./CheckAvi.vue";//+
-import roomSec from "./RoomSection.vue";// ?
+import roomSec from "./RoomSection.vue";//+
 import serviceSec from "./Services.vue";//+
 import contactSec from "./ContactUs.vue";//+
 
