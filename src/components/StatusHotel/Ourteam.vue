781878e68563a2ea49c848dc5f14042189764adb
anpdgr <ant-dgr@hotmail.com>
2020-05-16 22:16:33 +0700

    pull

--- src/components/StatusHotel/Ourteam.vue
+++ src/components/StatusHotel/Ourteam.vue
@@ -1,22 +1,24 @@
 <template>
   <div>
     <STnav />
-    <Side />
-
+    <Side/>
     <div id="all">
       <div id="summary" style="float:right">
         <!-- ปุ่มดู summary staff position -->
-      <b-button id="show-btn"  variant="success"   @click="showModalstaff">see summary</b-button>
+        <b-button id="show-btn" variant="success" @click="showModalstaff"
+          >see summary</b-button
+        >
         <!-- chart detail  -->
-      <b-modal ref="my-modal-staff" hide-footer title="Summary" size='lg'>
-        <div class="chart-wrapper">
-          <chart :options="chartOptionsBar" style="float:center"></chart>
-        </div>
-      </b-modal>
-    </div>
-      <h1 style="margin-top:30px">Staff information</h1>
-      <b-container fluid>
-        <!-- User Interface controls -->
+        <b-modal ref="my-modal-staff" hide-footer title="Summary" size="lg">
+          <div class="chart-wrapper">
+            <chart :options="chartOptionsBar" style="float:center"></chart>
+          </div>
+        </b-modal>
+      </div>
+      <div id="topSection">
+        <h1>Staff information</h1><br><br>
+        <b-container fluid>
+          <!-- User Interface controls -->
         <b-row>
           <b-col lg="6" class="my-1">
             <b-form-group
@@ -27,6 +29,7 @@
               label-for="filterInput"
               class="mb-0"
             >
+              <!-- input text for filter -->
               <b-input-group size="sm">
                 <b-form-input
                   v-model="filter"
@@ -34,6 +37,7 @@
                   id="filterInput"
                   placeholder="Type to Search"
                 ></b-form-input>
+                <!-- cleat text in filter box -->
                 <b-input-group-append>
                   <b-button :disabled="!filter" @click="filter = ''"
                     >Clear</b-button
@@ -52,15 +56,17 @@
               description="Leave all unchecked to filter on all data"
               class="mb-0"
             >
+              <!-- check box for filter  -->
               <b-form-checkbox-group v-model="filterOn" class="mt-1">
-                <b-form-checkbox value="id">ID</b-form-checkbox>
-                <b-form-checkbox value="sName">Name</b-form-checkbox>
-                <b-form-checkbox value="position">Position</b-form-checkbox>
-                <b-form-checkbox value="salary">Salary</b-form-checkbox>
+                  <b-form-checkbox value="Staff_ID">ID</b-form-checkbox>
+                  <b-form-checkbox value="Staff_FirstName">First name</b-form-checkbox>
+                  <b-form-checkbox value="Staff_LastName">Last name</b-form-checkbox>
+                  <b-form-checkbox value="Position">Position</b-form-checkbox>
+                  <b-form-checkbox value="Salary">Salary</b-form-checkbox>
               </b-form-checkbox-group>
             </b-form-group>
           </b-col>
-
+          <!-- setting number of row's booking data in per page -->
           <b-col sm="5" md="6" class="my-1">
             <b-form-group
               label="Per page"
@@ -81,115 +87,117 @@
             </b-form-group>
           </b-col>
         </b-row>
+        </b-container>
+      </div>
+      
+    </div>
 
-        <!-- Main table element -->
-        <b-table
-          show-empty
-          stacked="md"
-          :items="items"
-          :fields="fields"
-          :current-page="currentPage"
-          :per-page="perPage"
-          :filter="filter"
-          :filterIncludedFields="filterOn"
-          @filtered="onFiltered"
-          :tbody-tr-class="rowClass"
-        >
-          <template v-slot:cell(sName)="row">
-            {{ row.value.title }} {{ row.value.name }}
-          </template>
+    <div id="table" >
+      <b-table
+        show-empty
+        stacked="md"
+        :items="items"
+        :fields="fields"
+        :current-page="currentPage"
+        :per-page="perPage"
+        :filter="filter"
+        :filterIncludedFields="filterOn"
+        @filtered="onFiltered"
+        :tbody-tr-class="rowClass"
+        style="margin-top:110px"
+      >
+        <template v-slot:table-colgroup="scope">
+          <col
+            v-for="field in scope.fields"
+            :key="field.key"
+          />
+        </template>
+        <template v-slot:cell(actions)="row">
+          <b-button variant="light" size="sm" @click="row.toggleDetails">
+            {{ row.detailsShowing ? "Hide" : "Show" }} more details
+          </b-button>
 
-          <template v-slot:cell(actions)="row">
-            <b-button variant="light" size="sm" @click="row.toggleDetails">
-              {{ row.detailsShowing ? "Hide" : "Show" }} more details
-            </b-button>
+          <b-button
+            size="sm"
+            @click="showModal(row.item)"
+            class="mr-1"
+            variant="danger"
+          >
+            Edit
+          </b-button>
+        </template>
 
-            <b-button
-              size="sm"
-              @click="showModal(row.item)"
-              class="mr-1"
-              variant="danger"
-            >
-              Edit
-            </b-button>
-          </template>
-
-          <template v-slot:row-details="row">
-            <b-card>
-              <pre style="font-size:large; margin:0px">
+        <template v-slot:row-details="row">
+          <b-card>
+            <pre style="font-size:large; margin:0px">
               <b>Date of Birth</b>: {{row.item.Date_Of_Birth}}        <b>Address</b>: {{row.item.Address}}        <b>Start date</b>: {{row.item.Start_Date}}        <b>End date</b>: {{row.item.End_Date}}   
               </pre>
-            </b-card>
-          </template>
-        </b-table>
+          </b-card>
+        </template>
+      </b-table>
 
+      <!-- Info edit modal ปล ถ้า  placeholder เป็นข้อมูลปัจจุบันก็ดีนะ ทุก edit-->
+      <b-modal ref="editModal" title="Edit staff information" hide-footer>
+        <div id="editST">
+          <!-- {{infoModalContent}} -->
+          <b-form-group label="Position:" label-for="ip-position">
+            <b-form-input
+              id="ip-position"
+              v-model="formEdit.Position"
+              placeholder="Enter new position"
+            ></b-form-input>
+          </b-form-group>
+          <!-- {{infoModalContent.Salary}} -->
+          <b-form-group label="Salary:" label-for="ip-salary">
+            <b-form-input
+              id="ip-salary"
+              v-model="formEdit.Salary"
+              placeholder="Enter new salary"
+              required=""
+            ></b-form-input>
+          </b-form-group>
+          <!-- {{infoModalContent.Start_Date}} -->
+          <b-form-group label="Start date:" label-for="ip-sDate">
+            <b-form-datepicker
+              id="ip-sDate"
+              v-model="formEdit.SDate"
+              placeholder="-- select date --"
+            ></b-form-datepicker>
+          </b-form-group>
+          <!-- {{infoModalContent.End_Date}} -->
+          <b-form-group label="End date:" label-for="ip-eDate">
+            <b-form-datepicker
+              id="ip-eDate"
+              v-model="formEdit.EDate"
+              placeholder="-- select date --"
+            ></b-form-datepicker>
+          </b-form-group>
 
-        <!-- Info edit modal ปล ถ้า  placeholder เป็นข้อมูลปัจจุบันก็ดีนะ ทุก edit-->
-          <b-modal ref="editModal" title="Edit staff information" hide-footer>
-              <div id="editST">
-                <!-- {{infoModalContent}} -->
-                <b-form-group label="Position:" label-for="ip-position">
-                  <b-form-input
-                    id="ip-position"
-                    v-model="formEdit.Position"
-                    placeholder="Enter new position" 
-                  
-                  ></b-form-input>
-                </b-form-group>
-                <!-- {{infoModalContent.Salary}} -->
-                <b-form-group label="Salary:" label-for="ip-salary">
-                  <b-form-input
-                    id="ip-salary"
-                    v-model="formEdit.Salary"
-                    placeholder="Enter new salary"
-                    required=""
-                  ></b-form-input>
-                </b-form-group>
-                <!-- {{infoModalContent.Start_Date}} -->
-                <b-form-group label="Start date:" label-for="ip-sDate">
-                  <b-form-datepicker
-                    id="ip-sDate"
-                    v-model="formEdit.SDate"
-                    placeholder="-- select date --"
-                  ></b-form-datepicker>
-                </b-form-group>
-                <!-- {{infoModalContent.End_Date}} -->
-                <b-form-group label="End date:" label-for="ip-eDate">
-                  <b-form-datepicker
-                    id="ip-eDate"
-                    v-model="formEdit.EDate"
-                    placeholder="-- select date --"
-                  ></b-form-datepicker>
-                </b-form-group>
+          <div style="float: right;">
+            <b-button
+              type="reset"
+              variant="danger"
+              style="margin-right:20px;"
+              @click="onReset"
+              >Reset</b-button
+            >
+            <b-button @click="onSubmit" variant="primary">Submit</b-button>
+          </div>
+        </div>
+      </b-modal>
 
-                <div style="float: right;">
-                  <b-button
-                    type="reset"
-                    variant="danger"
-                    style="margin-right:20px;"
-                    @click="onReset"
-                    >Reset</b-button
-                  >
-                  <b-button @click="onSubmit" variant="primary"
-                    >Submit</b-button
-                  >
-                </div>
-              </div>
-          </b-modal>
-
-        <b-col class="my-1">
-          <b-pagination
-            v-model="currentPage"
-            :total-rows="totalRows"
-            :per-page="perPage"
-            pills
-            align="center"
-            class="my-0"
-          ></b-pagination>
-          <br />
-          <hr />
-        </b-col>
-      </b-container>
+      <b-col class="my-1">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="totalRows"
+          :per-page="perPage"
+          pills
+          align="center"
+          class="my-0"
+        ></b-pagination>
+        <br />
+        <hr />
+      </b-col>
     </div>
   </div>
 </template>
@@ -201,9 +209,9 @@ export default {
   components: { STnav, Side },
   data() {
     return {
-            chartOptionsBar: {
+      chartOptionsBar: {
         xAxis: {
-          data: ["IT", "housekeeper","DBA","dev"],
+          data: ["IT", "housekeeper", "DBA", "dev"],
         },
         yAxis: {
           type: "value",
@@ -211,7 +219,7 @@ export default {
         series: [
           {
             type: "bar",
-            data: [63, 5,45,9],
+            data: [63, 5, 45, 9],
           },
         ],
         title: {
@@ -223,7 +231,7 @@ export default {
         },
         color: ["#127ac2"],
       },
-      // staff info 
+      // staff info
       items: [],
       fields: [
         { key: "Staff_ID", label: "Staff ID", sortable: true },
@@ -246,27 +254,23 @@ export default {
       currentPage: 1,
       perPage: 5,
       pageOptions: [5, 10, 15],
-      sortBy: "",
-      sortDesc: false,
-      sortDirection: "asc",
       filter: null,
       filterOn: [],
       //edit prmt
-      formEdit:{
+      formEdit: {
         staffid: null,
         Position: null,
         Salary: null,
         SDate: null,
-        EDate: null
+        EDate: null,
       },
       show: true,
-      
     };
   },
   computed: {
     totalRows() {
-        return this.items.length
-      },
+      return this.items.length;
+    },
     sortOptions() {
       // Create an options list from our fields
       return this.fields
@@ -280,7 +284,7 @@ export default {
     // Set the initial number of items
     this.totalRows = this.items.length;
     this.fetchStaff();
-   //this.MergeName();
+    //this.MergeName();
   },
   methods: {
     // MergeName(){
@@ -294,13 +298,13 @@ export default {
       this.currentPage = 1;
     },
     showModal(item) {
-      this.infoModalContent=item;
+      this.infoModalContent = item;
       console.log(this.infoModalContent.Staff_ID);
-      this.formEdit.staffid=this.infoModalContent.Staff_ID;
-      this.formEdit.Position=this.infoModalContent.Position;
-      this.formEdit.Salary=this.infoModalContent.Salary;
-      this.formEdit.SDate=this.infoModalContent.Start_Date;
-      this.formEdit.EDate=this.infoModalContent.End_Date;
+      this.formEdit.staffid = this.infoModalContent.Staff_ID;
+      this.formEdit.Position = this.infoModalContent.Position;
+      this.formEdit.Salary = this.infoModalContent.Salary;
+      this.formEdit.SDate = this.infoModalContent.Start_Date;
+      this.formEdit.EDate = this.infoModalContent.End_Date;
       this.$refs["editModal"].show();
     },
     hideModal() {
@@ -333,73 +337,75 @@ export default {
         this.show = true;
       });
     },
-    fetchStaff(){
+    fetchStaff() {
       var formData = this.toFormData(this.items);
       this.axios
-      .post(
-          "http://hakuna-hotel.kmutt.me/phpapi/Staff.php?action=read",formData
+        .post(
+          "http://hakuna-hotel.kmutt.me/phpapi/Staff.php?action=read",
+          formData
         )
-        .then(response => {
+        .then((response) => {
           //set var to default
-          this.items=response.data.data;
+          this.items = response.data.data;
           if (response.data.error) {
             console.log(response.data.error);
           } else {
             console.log(response.data.message);
           }
         });
-      },
+    },
     updateStaff() {
       console.log(this.formEdit.EDate);
-      if(this.formEdit.EDate==null){
+      if (this.formEdit.EDate == null) {
         var formData1 = this.toFormData(this.formEdit);
         this.axios
-        .post(
-          "http://hakuna-hotel.kmutt.me/phpapi/editourteam.php?action=update",formData1
-        )
-        .then(response => {
-          //set var to default
-          console.log(response);
-          this.formEdit = {
-            staffid: null,
-            Position: null,
-            Salary: null,
-            SDate: null,
-            EDate: null
-          };
-          if (response.data.error) {
-            console.log(response.data.error);
-          } else {
-            console.log(response.data.message);
-          }
-        });
-      }
-      else{
+          .post(
+            "http://hakuna-hotel.kmutt.me/phpapi/editourteam.php?action=update",
+            formData1
+          )
+          .then((response) => {
+            //set var to default
+            console.log(response);
+            this.formEdit = {
+              staffid: null,
+              Position: null,
+              Salary: null,
+              SDate: null,
+              EDate: null,
+            };
+            if (response.data.error) {
+              console.log(response.data.error);
+            } else {
+              console.log(response.data.message);
+            }
+          });
+      } else {
         var formData2 = this.toFormData(this.formEdit);
         this.axios
-        .post(
-          "http://hakuna-hotel.kmutt.me/phpapi/editourteam.php?action=updateED",formData2
-        )
-        .then(response => {
-          //set var to default
-          console.log(response);
-          this.formEdit = {
-            staffid: null,
-            Position: null,
-            Salary: null,
-            SDate: null,
-            EDate: null
-          };
-          if (response.data.error) {
-            console.log(response.data.error);
-          } else {
-            console.log(response.data.message);
-          }
-        });
+          .post(
+            "http://hakuna-hotel.kmutt.me/phpapi/editourteam.php?action=updateED",
+            formData2
+          )
+          .then((response) => {
+            //set var to default
+            console.log(response);
+            this.formEdit = {
+              staffid: null,
+              Position: null,
+              Salary: null,
+              SDate: null,
+              EDate: null,
+            };
+            if (response.data.error) {
+              console.log(response.data.error);
+            } else {
+              console.log(response.data.message);
+            }
+          });
       }
     },
-      // convert to formdata
-      toFormData(obj) {
+    // convert to formdata
+    toFormData(obj) {
       var fd = new FormData();
       for (var i in obj) {
         fd.append(i, obj[i]);
@@ -419,11 +425,11 @@ export default {
       else return;
     },
     showModalstaff() {
-        this.$refs['my-modal-staff'].show()
-      },
-      hideModalstaff() {
-        this.$refs['my-modal-staff'].hide()
-      },
+      this.$refs["my-modal-staff"].show();
+    },
+    hideModalstaff() {
+      this.$refs["my-modal-staff"].hide();
+    },
   },
 };
 </script>
